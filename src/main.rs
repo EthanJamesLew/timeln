72f6db5b23dda48a29e1c81a7e5d37641acbe1a9
Ethan Lew <elew@galois.com>
2023-06-14 16:19:18 -0700

    start docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+//! This is a command-line utility written in Rust that reads input from stdin line by line,
+//! and prints out the time elapsed since the start of the program and the delta time between lines or regex matches.
+//!
+//! It provides options to enable colorization of the timing information and to match lines based on a regex pattern.
+//! The timing is colorized green by default, and matched strings are colorized red.
+//! If no regex pattern is provided, the program times every line.
+//! If a regex pattern is provided, it times and prints only the lines that match the regex pattern.
+//!
+//! The utility uses the StructOpt crate for parsing command line arguments and the regex crate for matching regular expressions.
+//! It also uses the colored crate to colorize the output.
+//!
+//! Usage: 
+//!     To use this utility, compile it and run it from the command line. 
+//!     You can provide input directly from the command line or pipe input from another command.
+//!
+//!     You can use the `-c` or `--color` option to enable colorization of the timing information.
+//!     Use the `-r` or `--regex` option followed by a regex pattern to time and print only the lines that match the pattern.
+//!
+//! Example:
+//!     echo "Hello\nWorld" | time-reader
+//!     echo "Hello\nWorld" | time-reader -c
+//!     echo "Hello\nWorld" | time-reader -r "World"
+//!     echo "Hello\nWorld" | time-reader -c -r "World"
+//!
+//! The script prints the elapsed time and the delta time between lines or regex matches in the format "[time: XX.XX s, delta: XX.XX s]".
+//! If colorization is enabled, the timing information is printed in green and the matched strings are printed in red.
+
 use std::io::{self, BufRead};
 use std::time::{Instant, Duration};
 use colored::*;
